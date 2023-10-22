2882d9e609172fadedaa35200ee73dbbaa6bfd01
CarbajalB <143287166+CarbajalB@users.noreply.github.com>
2023-10-22 10:57:07 -0600

    Tratamiento Probabilístico Del Lenguaje

---
+++ _12_Tratamiento_probabilistico_del_lenguaje/_4_Recuperación_de_Datos.py
@@ -0,0 +1,20 @@
+empleados = {
+    101: {"nombre": "Juan", "departamento": "Ventas"},
+    102: {"nombre": "Ana", "departamento": "TI"},
+    103: {"nombre": "Carlos", "departamento": "RRHH"},
+    104: {"nombre": "Luisa", "departamento": "Ventas"}
+}
+
+# Función para recuperar datos de empleados por número de empleado
+def recuperar_empleado(numero_empleado):
+    empleado = empleados.get(numero_empleado)
+    if empleado:
+        return empleado
+    else:
+        return "Empleado no encontrado"
+
+# Recuperar datos de empleados
+numero_empleado = 102
+empleado = recuperar_empleado(numero_empleado)
+print(f"Datos del empleado con numero {numero_empleado}: {empleado}")
+
---
+++ _12_Tratamiento_probabilistico_del_lenguaje/_5_Extracción_de_Información.py
@@ -0,0 +1,30 @@
+import nltk
+from nltk import ne_chunk
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+text = "Barack Obama was born in Honolulu, Hawaii. He served as the 44th President of the United States."
+
+# Tokenización de oraciones y palabras
+sentences = sent_tokenize(text)
+words = [word_tokenize(sentence) for sentence in sentences]
+
+# Etiquetado de entidades nombradas (NER)
+tagged = [nltk.pos_tag(word) for word in words]
+entities = [ne_chunk(tag) for tag in tagged]
+
+# Función para extraer nombres de personas y lugares
+def extract_info(tree):
+    info = {"person": [], "location": []}
+    for subtree in tree:
+        if type(subtree) == nltk.Tree:
+            if subtree.label() == 'GPE':  # Lugar
+                info["location"].append(" ".join([token for token, pos in subtree.leaves()]))
+            elif subtree.label() == 'PERSON':  # Persona
+                info["person"].append(" ".join([token for token, pos in subtree.leaves()]))
+    return info
+
+# Realizar la extracción de información
+info = extract_info(entities)
+
+print("Nombres de personas:", info["person"])
+print("Lugares:", info["location"])
